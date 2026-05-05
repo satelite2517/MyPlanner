e12ee818964ddd9c52cdf2b49a17603db7bdbc22
satelite2517 <satelite2517@gmail.com>
2026-05-05 18:57:44 +0900

    Initial Commit

---
+++ Planner/PlannerApp.swift
@@ -0,0 +1,32 @@
+//
+//  PlannerApp.swift
+//  Planner
+//
+//  Created by 이선재 on 5/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PlannerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
